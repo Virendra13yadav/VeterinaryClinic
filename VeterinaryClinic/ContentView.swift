6c6db361c81eec20320467fbbfad759501cd92f2
virendra yadav <virendra.yadav@hurix.com>
2025-11-26 00:21:44 +0530

    Initial project commit

---
+++ VeterinaryClinic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VeterinaryClinic
+//
+//  Created by Apple on 26/11/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VeterinaryClinic/VeterinaryClinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeterinaryClinicApp.swift
+//  VeterinaryClinic
+//
+//  Created by Apple on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VeterinaryClinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
